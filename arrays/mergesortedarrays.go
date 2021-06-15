c25d44c835732cf039c1339ebced8708edd418f6
Ajay Kamble <ajay.kamble@selfscore.com>
2021-06-15 12:54:26 +0530

    fixes to reduce execution time for merge sorted arrays

--- arrays/mergesortedarrays.go
+++ arrays/mergesortedarrays.go
@@ -3,13 +3,14 @@ package arrays
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	i, j, k := m-1, n-1, m+n-1
 	for ; k > -1; k-- {
-		if i > -1 && j > -1 {
-			if nums1[i] > nums2[j] {
-				nums1[k], i = nums1[i], i-1
-			} else {
-				nums1[k], j = nums2[j], j-1
-			}
-		} else if i > -1 {
+		nums1HasElems := i > -1
+		nums2HasElems := j > -1
+		if !nums2HasElems {
+			break
+		} else if !nums1HasElems {
+			copy(nums1[:k+1], nums2[:j+1])
+			break
+		} else if nums1[i] > nums2[j] {
 			nums1[k], i = nums1[i], i-1
 		} else {
 			nums1[k], j = nums2[j], j-1
