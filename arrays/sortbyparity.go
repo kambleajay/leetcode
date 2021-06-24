00648e74bfec19bebda725d6e82910fcca3482d8
Ajay Kamble <ajay.kamble@selfscore.com>
2021-06-24 23:14:25 +0530

    refactoring

--- arrays/sortbyparity.go
+++ arrays/sortbyparity.go
@@ -6,15 +6,14 @@ func sortArrayByParity(nums []int) []int {
 	}
 	lo, hi := 0, len(nums)-1
 	for ; lo < hi; {
-		if nums[lo] % 2 != 0 && nums[hi] % 2 == 0 {
-			nums[lo], nums[hi], lo, hi = nums[hi], nums[lo], lo+1, hi-1
-		} else {
-			if nums[lo] % 2 == 0 {
-				lo++
-			}
-			if nums[hi] % 2 != 0 {
-				hi--
-			}
+		if nums[lo]%2 != 0 && nums[hi]%2 == 0 {
+			nums[lo], nums[hi] = nums[hi], nums[lo]
+		}
+		if nums[lo]%2 == 0 {
+			lo++
+		}
+		if nums[hi]%2 != 0 {
+			hi--
 		}
 	}
 	return nums
